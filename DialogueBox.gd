d4930d1a43e5d09f8193b508f2d13534140905b3
Camille Sanchez <camsanch@macbookpro.home>
2026-07-12 00:07:10 +0200

    dialogue box OK + tests

--- DialogueBox.gd
+++ DialogueBox.gd
@@ -1,23 +1,39 @@
 extends Control
 
+signal dialogue_finished
+@onready var speaker_label = $Panel/VBoxContainer/SpeakerName
+@onready var dialogue_label = $Panel/VBoxContainer/DialogueText
 var dialogue_lines = []
 var current_index = 0
 
 func _ready():
-	pass
+	var test = [
+		{"speaker": "kiki", "text": "hahaha"},
+		{"speaker": "kiki", "text": "nounou il est gros"},
+		{"speaker": "kiki", "text": "mais je l'aime"}
+	]
+	start_dialogue(test)
+
 
 func start_dialogue(lines):
 	if lines:
 		dialogue_lines = lines
-	current_index = 0
-	visible = true
-	next_line()
+		current_index = 0
+		visible = true
+		next_line()
 	
 
 func next_line():
-	pass
+	if current_index < dialogue_lines.size():
+		speaker_label.text = dialogue_lines[current_index]["speaker"]
+		dialogue_label.text = dialogue_lines[current_index]["text"]
+		current_index += 1
+	else:
+		dialogue_finished.emit()
 
 func _input(event):
+	if not visible:
+		return
 	if event is InputEventMouseButton and event.pressed:
 		next_line()
 	if event is InputEventKey and event.pressed:
